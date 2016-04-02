79244bb47993bc260e04e3d9946a5e2f13628951
Zachary.smith <Zachary.smith@yodle.com>
2016-04-02 19:22:02 -0400

    Added face response

--- moods.py
+++ moods.py
@@ -2,11 +2,32 @@ import requests
 import json
 
 def main():
-    with open("key.txt", "r") as keys:
-        keysJson = json.loads(keys.read())
-        key = keysJson['key']        
-    print key
+    api_key = get_key()
+    if ( api_key == "YOUR_KEY_HERE") : 
+        print "Fill in your keys.txt properly doge." 
+        return 
+    
+    img_url = "http://1.images.comedycentral.com/images/shows/nightly_show/videos/season_2/02042/ns_02_042_03.jpg"
+    emotion_response = get_emotions(api_key, img_url)
+    print emotion_response
 
+def get_key(): 
+    with open("keys.txt", "r") as keys:
+        keysJson = json.loads(keys.read())
+        return keysJson['key']        
+        
+def get_emotions(api_key, img_as_binary):
+    request_url = "https://api.projectoxford.ai/emotion/v1.0/recognize"
+    img_url = "http://1.images.comedycentral.com/images/shows/nightly_show/videos/season_2/02042/ns_02_042_03.jpg"
+    
+    headers = {
+        'content-type' : 'application/json',
+        'Ocp-Apim-Subscription-Key' : api_key
+    }
+    payload = { "url": img_url}
+
+    response = requests.post(request_url, data=json.dumps(payload), headers=headers)
+    return response.json()
 
 if __name__ == '__main__':
     main()
